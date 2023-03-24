bc13f9d3ab2dc2f9bf15acb2a4a12089737dd71c
Fatih Metiner <fatihmetiner@outlook.com>
2023-03-24 09:59:02 +0300

    Add project files.

---
+++ EasyToDoApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasyToDoApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, Title = "Buy groceries", Done = false }, 
+                new TodoItem() { Id = 2, Title = "Clean the house", Done = false }, 
+                new TodoItem() { Id = 3, Title = "Take out the trash", Done = true }, 
+                new TodoItem() { Id = 4, Title = "Do laundry", Done = true });
+        }
+    }
+}
---
+++ EasyToDoApi/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyToDoApi.Data
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
